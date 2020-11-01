e0727094d13cdd473427c4349aef4a917e7d6367
sunshine-lin <1187181555@qq.com>
2020-11-02 01:09:42 +0800

    12

---
+++ pages/device/device.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		<button type="default">12</button>
+		<uni-badge text="12222" type="success"></uni-badge>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/list/list.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		列表页
+		<view class="item" v-for="(item,index) in list" :key="index">{{item}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: ['打了飞机','阿道夫','大是大非','打了飞机','阿道夫','大是大非','打了飞机','阿道夫','大是大非']
+			};
+		},
+		onPullDownRefresh() {
+			setTimeout(()=>{
+				this.list.push('23234adf ')
+				uni.stopPullDownRefresh()
+			},500)
+			console.log(11111)
+		},
+		onReachBottom() {
+			setTimeout(()=>{
+				this.list.push('23333333333 ')
+			},500)
+			console.log(22222222222)
+		}
+	}
+</script>
+
+<style lang="scss">
+.item{
+	line-height: 100rpx;
+}
+</style>
